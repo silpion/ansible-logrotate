354be1e18b6c3566825a4871b54ecd39559a1410
Mark Kusch <mark.kusch@gmail.com>
2015-01-28 16:47:19 +0100

    Add basic integration tests

--- spec/default/logrotate_spec.rb
+++ spec/default/logrotate_spec.rb
@@ -1,4 +1,16 @@
 require 'spec_helper'
 
+describe package('logrotate') do
+  it { should be_installed }
+end
+
+describe file('/etc/logrotate.d/ansible_logrotate_ansiblelogrotatetest') do
+  it { should be_file }
+  it { should be_owned_by 'root' }
+  it { should be_grouped_into 'root' }
+  it { should be_mode '644' }
+  its(:content) { should match 'Managed by Ansible' }
+end
+
 # write up RSpec integration tests here
 #   see: http://serverspec.org/resource_types.html
